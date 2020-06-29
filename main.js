cd431d079af8c319edb75c73e85c2eb52e66dc15
alec azimuth <azidyn@protonmail.com>
2020-06-29 14:32:52 +0100

    done lol

--- main.js
+++ main.js
@@ -1,24 +1,12 @@
 
 const Aggregate = require('./Aggregate');
 
-let agg = new Aggregate('./trade', '1d');
+let agg = new Aggregate('./trade', '1h');
 
 agg.on('bar', b => {
+
     console.log( b );
+    
 })
 
 agg.start('XBTUSD');
-
-
-// const Tick = require('./Tick');
-
-
-// let tick = new Tick('./trade');
-
-
-// tick.on('tick', t => {
-//     console.log( t );
-// })
-
-
-// tick.start('XBTUSD')
